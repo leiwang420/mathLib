f4454092de7a79392ecf392990c261440687afa1
wanglei <leiwang420@gmail.com>
2016-03-11 21:25:23 +0800

    update maths

--- MathLib/MathLib/Mathf.cpp
+++ MathLib/MathLib/Mathf.cpp
@@ -6,6 +6,7 @@ float Mathf::PI = 3.14159265358979f;
 float Mathf::Deg2Rad = PI * 2 / 360.0f;
 float Mathf::Rad2Deg = 360.0f / PI / 2;
 float Mathf::Infinity = FLT_MAX;
+float Mathf::NegativeInfinity = -FLT_MAX;
 
 float Mathf::deg2Rad(float d)
 {
@@ -15,4 +16,14 @@ float Mathf::deg2Rad(float d)
 float Mathf::rad2Deg(float r)
 {
 	return r * Rad2Deg;
+}
+
+bool Mathf::isPowerOf2(int i)
+{
+    if (i > 0) {
+        return ((i-1)&i) == 0;
+    }else if(i == 0)
+        return true;
+    else
+        return false;
 }
